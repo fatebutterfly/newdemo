d5e2bb8283f4ed83ed50d9dea6d3ef9c67b263db
fatebutterfly <yang.haijun@yahoo.com>
2016-08-26 12:26:33 +0800

    aaa

--- 2.js
+++ 2.js
@@ -16,3 +16,28 @@ connection.query("select * from beans_user",function(err,rows,fields){
 console.log("it runs to there");
 connection.end();
 
+var a[100][100];  
+var d[100][100];  
+var n;  
+function dfs(i,j)  
+{  
+    if(d[i][j]>0)  
+    return d[i][j];  
+    return d[i][j]=a[i][j]+(i==n?0:max(dfs(i+1,j),dfs(i+1,j+1)));  
+}  
+function main()  
+{  
+    while(scanf("%d",&n)!=EOF)  
+    {  
+        for(int i=1;i<=n;i++)  
+        {  
+            for(int j=1;j<=i;j++)  
+            scanf("%d",&a[i][j]);  
+        }  
+        memset(d,-1,sizeof(d));  
+        printf("%d\n",dfs(1,1));  
+    }  
+    return 0;  
+}
+
+
